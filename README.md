cd537b0d2a309e8d5f4fba3478ee1d8fd077633b
Colton Hurst <colton@coltonhurst.com>
2021-02-18 12:48:06 -0500

    Add basic threading example

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Different concepts and examples in the [C# programming language](https://docs.mi
 ## Building
 
 To build and run an example, just `cd` into the example you want, and run `dotnet run`. You should have the [.NET Core SDK](https://dotnet.microsoft.com/download/dotnet-core) installed.
+
+## Thanks
+
+Some of the examples have been based on example code and explanations from [C# 7.0 in a Nutshell](https://web.archive.org/web/20201111233013/https://www.oreilly.com/library/view/c-70-in/9781491987643/) (like the [basic threading example](./threads-basic/Program.cs)). Thanks to the authors for great explanations and code examples.
---
+++ threads-basic/Program.cs
@@ -0,0 +1,70 @@
+﻿/*
+    Here is a simple threading example in C#.
+
+    On a single core system, the OS must allocate "slices"
+    of time to each thread (on Windows around 20ms each) to
+    simulate concurrency, resulting in blocks of A's and B's.
+    
+    On a multi core system, the two threads can actually execute in parallel.
+    In this example we still get blocks of A's and B's due to subtleties
+    in how `Console` handles concurrent requests.
+
+    - A thread's `IsAlive` property will return true once it's started and until it ends.
+    - Once ended, a thread cannot restart. It ends when the delegate passed to the Thread's
+      constructor finishes executing.
+    - You can name threads for debugging purposes (`Name` property). You can only name it once.
+    - The static `Thread.CurrentThread` property gives the currently executing thread.
+      Ex: `Console.WriteLine(Thread.CurrentThread.Name);`
+*/
+
+using System;
+using System.Threading;
+
+namespace threads_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(WriteA);      // Kick off a new thread (passing in the WriteA func)
+            t.Start();                          // Start running SayHello()
+
+            // At the same time, do something different on the main thread (write some B's)
+            for (int i = 0; i < 1000; i++) {
+                Console.Write("B");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static void WriteA()
+        {
+            for (int i = 0; i < 1000; i++) {
+                Console.Write("A");
+            }
+        }
+    }
+}
+
+/*
+    The output will usually be a mix of A's and B's, like so:
+
+    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBABB
+    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBABBBBBBBBBBBBBBBBBABB
+    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+    BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+    BBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    AABAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    AAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+    BBBBBBBBBBBBBBBBBBBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAAAAAAABAAAAAAAAAAAAAAAA
+    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    AAAAAAAAAAAAAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+
+    ... etc (newlines in output added for readability)
+*/
